5738243c31853f937cc768b86c1a2f0173f2f820
Xu Chunyang <jyochinyo@hotmail.com>
2023-07-25 08:43:24 +0800

    检查node版本,工具sermver;colors;constant

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,14 +2,33 @@
 
 module.exports = core;
 
+const sermver = require('semver')
+const colors = require('colors/safe')
 const pkg = require('../package.json')
 const log = require('@jkfe-cli/log')
+const constant = require('./const')
 
 function core() {
-  checkPkgVersion()
+  try {
+    checkPkgVersion()
+    checkNodeVersion()
+  } catch (error) {
+    log.error(error.message)
+  }
 }
 
-// 检查版本
+// 检查node版本
+function checkNodeVersion() {
+  // 第一步: 获取当前node版本号
+  const currentVersion = process.version
+  // 第二步: 比对最低版本号
+  const lowestNodeVersion = constant.LOWEST_NODE_VERSION
+  if (!sermver.gte(currentVersion, lowestNodeVersion)) {
+    throw new Error(colors.red(`jkfe-cli 需要安装 v${lowestNodeVersion} 以上版本的 Node.js`))
+  }
+}
+
+// 检查脚手架版本
 function checkPkgVersion() {
   log.notice('cli', pkg.version)
 }
