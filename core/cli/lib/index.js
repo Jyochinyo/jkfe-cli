c8e00039c5569156de148ef14c28068c55f95cba
Xu Chunyang <jyochinyo@hotmail.com>
2023-07-25 11:34:44 +0800

    检查root账户,root降级,root-check

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,20 +1,34 @@
 'use strict';
 
+// improt rootCheck from 'root-check';
+
 module.exports = core;
 
 const sermver = require('semver')
 const colors = require('colors/safe')
 const pkg = require('../package.json')
 const log = require('@jkfe-cli/log')
-const constant = require('./const')
+const constant = require('./const');
 
 function core() {
   try {
     checkPkgVersion()
     checkNodeVersion()
+    checkRoot()
   } catch (error) {
     log.error(error.message)
   }
+}
+
+// 检查 root 账户
+function checkRoot() {
+  // root-check 版本过高 不支持require
+  const rc = import('root-check')
+  rc.then((rootCheck) => {
+    rootCheck?.default()
+    // 打印账号信息
+    console.log('当前用户信息', process.geteuid())
+  })
 }
 
 // 检查node版本
